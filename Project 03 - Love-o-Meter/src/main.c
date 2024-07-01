a0dced5523d93fc8fd793b0ace683ca2ade2316f
IcePanorama <thriftedroot@protonmail.com>
2024-07-01 17:42:50 -0400

    Refactored main.c.

--- Project 03 - Love-o-Meter/src/main.c
+++ Project 03 - Love-o-Meter/src/main.c
@@ -19,6 +19,12 @@
 static void setup_love_o_meter (void);
 static void setup_serial_connection (void);
 static void main_loop (void);
+static void output_sensor_value (uint16_t sensor_value,
+                                 uint8_t *output_buffer);
+static float TMP36_sensor_value_to_voltage (uint16_t sensor_value);
+static void output_voltage (float voltage, uint8_t *output_buffer);
+static float voltage_to_temperature (float voltage);
+static void output_temperature (float temperature, uint8_t *output_buffer);
 
 int
 main (void)
@@ -36,9 +42,9 @@ main (void)
  *  Configures Port C as follows:
  *    * DDC0 (PORTC0) as input for TMP36
  *  Configures Port D as follows:
- *    * DDD2 (PORTD2) as output for green LED
- *    * DDD3 (PORTD3) as output for first red LED
- *    * DDD4 (PORTD4) as output for second red LED
+ *    * DDD2 (PORTD2) as output_buffer for green LED
+ *    * DDD3 (PORTD3) as output_buffer for first red LED
+ *    * DDD4 (PORTD4) as output_buffer for second red LED
  */
 static void
 setup_love_o_meter (void)
@@ -77,45 +83,74 @@ main_loop (void)
   //  do it either at startup or on a button press
   // const float BASELINE_TMP = 20.0;
 
-  const uint8_t sensor_value_label[] = "Sensor value: ";
-  const uint8_t voltage_label[] = "Voltage: ";
-  const uint8_t temperature_label[] = "Temperature (C): ";
-
   while (1)
     {
-      uint8_t output[STR_MAX_LEN];
-
-      /* Sensor value */
-      uart_send_string (sensor_value_label);
+      uint8_t output_buffer[STR_MAX_LEN];
 
       uint16_t sensor_value = adc_start (1);
-      itoa (sensor_value, (char *)output, 10);
-      uint8_t len = strlen ((char *)output);
-      output[len] = '\t';
-      output[len + 1] = '\0';
-      uart_send_string (output);
+      output_sensor_value (sensor_value, output_buffer);
 
-      /* Voltage */
-      uart_send_string (voltage_label);
+      float voltage = TMP36_sensor_value_to_voltage (sensor_value);
+      output_voltage (voltage, output_buffer);
 
-      float voltage = (sensor_value / 1024.0f) * 5.0f;
-      ftoa (voltage, output, 2, STR_MAX_LEN);
-      len = strlen ((char *)output);
-      output[len] = '\t';
-      output[len + 1] = '\0';
-      uart_send_string (output);
-
-      /* Temperature */
-      uart_send_string (temperature_label);
-
-      float temperature = (voltage - 0.5) * 100;
-      ftoa (temperature, output, 2, STR_MAX_LEN);
-      len = strlen ((char *)output);
-      output[len] = '\n';
-      output[len + 1] = '\r';
-      output[len + 2] = '\0';
-      uart_send_string (output);
+      float temperature = voltage_to_temperature (voltage);
+      output_temperature (temperature, output_buffer);
 
       _delay_ms (1000);
     }
+}
+
+static void
+output_sensor_value (uint16_t sensor_value, uint8_t *output_buffer)
+{
+  const uint8_t sensor_value_label[] = "Sensor value: ";
+  uart_send_string (sensor_value_label);
+
+  itoa (sensor_value, (char *)output_buffer, 10);
+  uint8_t len = strlen ((char *)output_buffer);
+  output_buffer[len] = '\t';
+  output_buffer[len + 1] = '\0';
+
+  uart_send_string (output_buffer);
+}
+
+static float
+TMP36_sensor_value_to_voltage (uint16_t sensor_value)
+{
+  return (sensor_value / 1024.0f) * 5.0f;
+}
+
+static void
+output_voltage (float voltage, uint8_t *output_buffer)
+{
+  const uint8_t voltage_label[] = "Voltage: ";
+  uart_send_string (voltage_label);
+
+  ftoa (voltage, output_buffer, 2, STR_MAX_LEN);
+  int len = strlen ((char *)output_buffer);
+  output_buffer[len] = '\t';
+  output_buffer[len + 1] = '\0';
+
+  uart_send_string (output_buffer);
+}
+
+static float
+voltage_to_temperature (float voltage)
+{
+  return (voltage - 0.5) * 100;
+}
+
+static void
+output_temperature (float temperature, uint8_t *output_buffer)
+{
+  const uint8_t temperature_label[] = "Temperature (C): ";
+  uart_send_string (temperature_label);
+
+  ftoa (temperature, output_buffer, 2, STR_MAX_LEN);
+  int len = strlen ((char *)output_buffer);
+  output_buffer[len] = '\n';
+  output_buffer[len + 1] = '\r';
+  output_buffer[len + 2] = '\0';
+
+  uart_send_string (output_buffer);
 }
